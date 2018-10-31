7cbf9fca2293023c7a89ba95a43f623adf1b5678
jusebastian <jusebastian13@gmail.com>
2018-10-30 20:38:56 -0500

    noveno commit

--- js/valFormDocumentSetData.js
+++ js/valFormDocumentSetData.js
@@ -1,23 +1,30 @@
 $(document).on('ready', function(){
   
-    //Funciòn para validar y traer datos a un select-----------------------------
+  //Funciòn para validar y traer datos a un select-----------------------------
   //Funcion Retorne un booleano ValidarEmail
   //$('#categoria-id').empty() //deja en vacio los campos
 
+  $("#categoria").empty(); //deja en vacio los campos
 
   $.ajax({
 
     type: "GET",
     url: "/api/categoria/all",
-    sucess: function(data){
+    success: function(data){
+      console.log(data);
       //Retorna Json data
-       
-      //iterando los valores del JSON
-      $.each(opciones, function(index , value){
+      //Iterando los valores de JSON
+      for (var i = 0; i < data.length; i++){
+        var id = data[i];
         //inserte el contenido, especificado por el parámetro,
-        $('#categoria').append('<option value=">' + value.id + '">' + value.nombre + '</option>');
-      });
+        $('#categoria').append('<option value=">' + id.id + '">' + id.nombre + '</option>');
+      }
+      
+    },
+    error: function (e){
+      console.log(e);
     }
+    
 
   });
 
